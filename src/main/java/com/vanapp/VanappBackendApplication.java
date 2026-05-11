5582c18c22f14918ff823d60f91fead9cbd782d2
pethersonzada <pethersonzada@gmail.com>
2026-05-11 09:08:07 -0300

    Mudando versões de dependências

--- src/main/java/com/vanapp/VanappBackendApplication.java
+++ src/main/java/com/vanapp/VanappBackendApplication.java
@@ -1,13 +1,16 @@
 package com.vanapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VanappBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VanappBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        SpringApplication.run(VanappBackendApplication.class, args);
+    }
 }
